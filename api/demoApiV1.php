c76ecb6d880754c58f228e65b1aa1afb54802d22
Deep Sohelia <soheliadeep@gmail.com>
2014-03-27 13:45:14 +0000

    Update demoApiV1.php

--- api/demoApiV1.php
+++ api/demoApiV1.php
@@ -1,11 +1,4 @@
 <?php
-// This is the server-side script
 $person = $_GET["name"];
-
-// Set the content type
-header('Content-Type: text/html');
- 
-// Send the data back
-
 echo "Hello $person !"
 ?>
