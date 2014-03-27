c204c4723c164ed6bac4019e01fba82f59b6292f
Deep Sohelia <soheliadeep@gmail.com>
2014-03-27 13:28:06 +0000

    Update demoApiV1.php

--- api/demoApiV1.php
+++ api/demoApiV1.php
@@ -1,11 +1,11 @@
 <?php
 // This is the server-side script
-$hardLad = $_GET["name"];
+$person = $_GET["name"];
 
 // Set the content type
 header('Content-Type: text/html');
  
 // Send the data back
 
-echo "Look out, Hard Lad $hardLad is in da house"
+echo "Hello $person !"
 ?>
