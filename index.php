804142f3f44457ef17dfa439f674f433b216a72a
isaacrg <isaac.reid.guest@gmail.com>
2014-04-05 15:13:27 +0000

    Testing work..

--- index.php
+++ index.php
@@ -4,12 +4,27 @@
         <link rel="stylesheet" type="text/css" href="/assets/css/base.css" />
     </head>
     <body>
-        <div class="clear">
-          <div class="col col-8 tablet-col-9 mobile-col-1-2">
-            <h1>Hasreon</h1>
+     <div class="header dark-blue-row">
+      <div class="container row-4 tablet-row-3 mobile-row-            <div class="clear mobile-center       
+          <a href="/base/" cBlass="logo left mobile-no-float uppercase">Hasreon</a>           <ul class="navigation inline-list ri                            <li><a class="active" href="/        li>
+                <li><a href="/client   Client</li>
+   
+              <li><a href="http://bit.ly/hasreon">Github</a></li>   </            </div>
           </div>
-          <div class="col col-4 tablet-col-3 mobile-col-1-2">
-            <?php
+          <d        ntainer mobile-containe                 <div class="banner clear">
+              <div class="left col-5 tablet-col-10 tablet-container tablet-center-tex            bile-container mobile-center-text">
+                  <h1 class="h3">A work in progress MUD/RPG game.>
+                     "thin row tablet-col-9 tablet-container">
+                  Open-Sourced and readily-deployable, see it on Github.     </p>
+                <div class="buttons row clear">
+                  <a href="https://github.com/matthewhartman/base/zipball/              ton green-button tablet-col-1-3 mobile-full">Download</a>
+                            /do          tto          -3 mobile-full">View Docs</a>
+                <                              div    device right tablet-full mobile-full">
+              </div>
+            </div>
+          </div>
+        </div>
+<?php
                 if($_GET['r']==$_ENV["INV"]){
             ?>
               <p>Goto <a href="/client/">client</a>?</p>
@@ -20,6 +35,12 @@
             <?php        
                 }
             ?>
+       <!--<div class="clear">
+          <div class="col col-8 tablet-col-9 mobile-col-1-2">
+            <h1>Hasreon</h1>
+          </div>
+          <div class="col col-4 tablet-col-3 mobile-col-1-2">
+            
           </div>
         </div>
         <div class="clear">
@@ -29,6 +50,6 @@
           <div class="col col-8 tablet-col-3 mobile-full">
             <p>Hasreon is an open-source RPG/MUD game, on Github.</p>
           </div>
-        </div>
+        </div>-->
     </body>
 </html>
