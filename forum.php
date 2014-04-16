e2d6857c1157ad41014cb0ce657cfa920c72b406
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-04-16 11:38:37 +0100

    Should work better, fixed missing phrase marks

--- forum.php
+++ forum.php
@@ -1,6 +1,6 @@
 <html><body>
 <?php
-if($_REQUEST["is"]==insane){
+if($_REQUEST["is"]=="insane"){
 ?>
 <script type="text/javascript" src="http://aiz3boe0p4.embed.talkiforum.com/embed/1.js"></script><div style="font-size:80%; text-align:center;" id="aiz3boe0p4t4lk1prm0">get your own <a href="http://talkiforum.com?utm_source=install&utm_medium=link&utm_campaign=get_your_own">embeddable forum</a> with Talki</div>
 <?php
