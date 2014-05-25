0bec7a609a43e8bf14b0028e22bd0d80ca41abb1
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-05-25 17:19:32 +0000

    Added page handler

---
+++ page.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+
+<html lang="en">
+<!--<![endif]-->
+
+<head>
+  <!-- Basic Page Needs
+  ================================================== -->
+  <meta charset="utf-8">
+
+  <title>Your Page Title Here :)</title>
+  <meta content="" name="description">
+  <meta content="" name="author"><!-- Mobile Specific Metas
+  ================================================== -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1" name=
+  "viewport"><!-- CSS
+  ================================================== -->
+  <link href="assets/css/skeleton/base.css" rel="stylesheet">
+  <link href="assets/css/skeleton/skeleton.css" rel="stylesheet">
+  <link href="assets/css/skeleton/layout.css" rel="stylesheet"><!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+  <div class="container">
+    
+    <div class="sixteen columns">
+      <h1 class="remove-bottom" style="margin-top: 40px">Hasreon</h1>
+
+      <h5>Version 0.5 beta</h5>
+      <hr>
+    </div>
+
+    <div class="sixteeen columns">
+      <?php echo $_GET['id']; ?>
+    </div>
+
+  </div>
+</body>
+</html>
