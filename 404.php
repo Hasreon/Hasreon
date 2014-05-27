53940f51defc9742a49a2cc7b874a19cf9df1cdc
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-05-27 11:23:46 +0100

    New 404 page!

--- 404.php
+++ 404.php
@@ -1,9 +1,40 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+
+<html lang="en">
+<!--<![endif]-->
+
+<head>
+  <!-- Basic Page Needs
+  ================================================== -->
+  <meta charset="utf-8">
+
+  <title>Hasreon</title>
+  <meta content="" name="description">
+  <meta content="" name="author"><!-- Mobile Specific Metas
+  ================================================== -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1" name=
+  "viewport"><!-- CSS
+  ================================================== -->
+  <link href="/assets/css/skeleton/base.css" rel="stylesheet">
+  <link href="/assets/css/skeleton/skeleton.css" rel="stylesheet">
+  <link href="/assets/css/skeleton/layout.css" rel="stylesheet"><!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+  <div class="container">
+
 <?php
-if($_REQUEST['go']==home){
-    header('Location: /');
-}else{
+echo file_get_contents("header.html");
 ?>
-Page not found. Fancy going <a href='?go=home'>home</a>?";
-<?php
-}
-?>
+    <div class="two-thirds column">
+			Page not found. Fancy going <a href='/'>home</a>?
+    </div>
+  </div>
+</body>
+</html>
