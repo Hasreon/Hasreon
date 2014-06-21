6f0eb105a2305e5af787b8e73a32e4aa8750a238
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-06-21 10:49:52 +0100

    Create api.php

---
+++ client/api.php
@@ -0,0 +1,13 @@
+<?php
+$path = $_SERVER['PATH_INFO'];
+switch($path){
+	case "move":
+		echo "i equals 0";
+		break;
+	case "description":
+		echo "i equals 1";
+		break;
+	default:
+		echo "What are you doing?";
+}
+?>
