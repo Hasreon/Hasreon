664f43d33de44087a83102d5214f66d1d1ebb85d
Deep Sohelia <soheliadeep@gmail.com>
2014-07-01 19:58:46 +0100

    Create backend.js

---
+++ api/backend.js
@@ -0,0 +1,26 @@
+var pubnub = PUBNUB.init({
+	publish_key: 'pub-c-f1c1301a-f5c5-4038-8f35-a4ec81044399',
+	subscribe_key: 'sub-c-2ed8a5ea-014a-11e4-b97b-02ee2ddab7fe'
+});
+
+pubnub.subscribe({
+	channel: 'hasreon_chat',
+	message: function (m) {
+		interpret(m);
+	}
+});
+
+var msgArray;
+
+function interpret(recv) {
+	msgArray = recv.split("~");
+	//Separate the Username from the incoming command
+	if(recv.charAt(0) == "/") {
+		//if the recieved data is a command, then do the below, else ignore.
+		//Note that commands will not support additional parameters
+		
+		if(msgArray[0] == "/n" || "/north" || "/N" || "/North" || "/NORTH") {
+			
+		}
+	}
+}
