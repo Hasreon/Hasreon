25f86de3ee4ccd5f0fac9d01db8b133ea01f7915
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-07-01 18:29:48 +0100

    Update api-v1.php

--- api/api-v1.php
+++ api/api-v1.php
@@ -1,12 +1,11 @@
 <?php
-include "../core/controller/player.php";
 $x=$_REQUEST;
-switch($_REQUEST["mode"]){
+switch($x["mode"]){
 	case "test":
 		echo "good";
 		break;
     case "move":
-        if(canMove($_REQUEST['move_direction'],$_REQUEST['current_location'])){
+        if(canMove($x['move_direction'],$x['current_location'])){
             respond("success");
         }else{
             respond("Can't move that direction!");
