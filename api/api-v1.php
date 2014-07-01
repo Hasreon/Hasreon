9bb04172d47ef91e91990f5258305308bd6b078a
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-07-01 18:47:17 +0100

    Update api-v1.php

--- api/api-v1.php
+++ api/api-v1.php
@@ -5,12 +5,9 @@ switch($x["mode"]){
 	case "test":
 		echo "good";
 		break;
-    case "move":
-        if(canMove($x['move_direction'],$x['current_location'])){
-            respond("success");
-        }else{
-            respond("Can't move that direction!");
-        }
+	case "move":
+		return canMove($x['move_direction'],$x['current_location']);
+		break;
 	default:
 		echo "No recognised mode requested.";
 		break;
