bdd144e744ffcd3b569861cdf7a92c5d053e6b56
Deep Sohelia <soheliadeep@gmail.com>
2014-07-02 19:19:20 +0100

    Update backend.js

--- api/backend.js
+++ api/backend.js
@@ -15,18 +15,18 @@ var msgArray;
 function bounce(recv) {
 	msgArray = recv.split("~");
 	//Separate the Username from the incoming command
-	if(recv.charAt(0) == "/") {
+	if (recv.charAt(0) == "/") {
 		//if the recieved data is a command, then do the below, else ignore.
 		//Note that commands will not support additional parameters
-		if(msgArray[0] == "/init") {
+		if (msgArray[0] == "/init") {
 			pubnub.publish({
 				channel: "hasreon_chat",
 				message: ":get('fairfalcongate')" + "~" + msgArray[1]
 			});
 		}
-		
-		if(msgArray[0] == "/n" || "/north" || "/N" || "/North" || "/NORTH") {
-			
+
+		if (msgArray[0] == "/n" || "/north" || "/N" || "/North" || "/NORTH") {
+
 		}
 	}
 }
