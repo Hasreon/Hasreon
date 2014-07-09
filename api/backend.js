6479f83b49264c49fdffdc6371b5331060a7ba1b
Deep Sohelia <soheliadeep@gmail.com>
2014-07-09 19:38:45 +0100

    Update backend.js

--- api/backend.js
+++ api/backend.js
@@ -98,34 +98,34 @@ function bounce(recv) {
 			gamelog.innerHTML = gamelog.innerHTML + "There is no Boulder Here...";
 		} else if (msgArray[2] == eval("fairfalconforest") && (!sword || !mayor)) {
 			gamelog.innerHTML = gamelog.innerHTML + "You are not ready...";
-		} else if(msgArray[2] == eval("fairfalconforest") && boulder) {
+		} else if (msgArray[2] == eval("fairfalconforest") && boulder) {
 			gamelog.innerHTML = gamelog.innerHTML + "The Boulder is not in the way...";
 		}
-		
+
 		if (msgArray[0] == "/blacksmith" && mayor && msgArray[2] == eval("fairfalconblacksmith") && !sword) {
 			gamelog.innerHTML = gamelog.innerHTML + "Oh, its you. The Mayor told me about you. You wanted a sword? It's there on the counter.";
 			gamelog.innerHTML = gamelog.innerHTML + "<br /> You take the sword. <br />";
 			sword = true
-		} else if(msgArray[0] == "/blacksmith" && msgArray[2] == eval("fairfalconmainstreet") && !mayor) {
+		} else if (msgArray[0] == "/blacksmith" && msgArray[2] == eval("fairfalconmainstreet") && !mayor) {
 			gamelog.innerHTML = gamelog.innerHTML + "The blacksmith is out...";
-		} else if(msgArray[0] == "/blacksmith" && msgArray[2] !== eval("fairfalconmainstreet")) {
+		} else if (msgArray[0] == "/blacksmith" && msgArray[2] !== eval("fairfalconmainstreet")) {
 			gamelog.innerHTML = gamelog.innerHTML + "There is no blacksmith here...";
-		} else if(msgArray[0] == "/blacksmith" && msgArray[2] == eval("fairfalconmainstreet") && sword) {
+		} else if (msgArray[0] == "/blacksmith" && msgArray[2] == eval("fairfalconmainstreet") && sword) {
 			gamelog.innerHTML = gamelog.innerHTML + "<br /> There is nothing here. <br />";
 		}
-		
+
 		if (msgArray[0] == "/mayor" && msgArray[2] == eval("fairfalcontownhall") && !mayor) {
 			gamelog.innerHTML = gamelog.innerHTML + "<br /> Oh its you. I've heard many things about you. Please, have a seat. <br /> You sit down. <br /> I have a problem. In the forest there is a tpye of sea-dwelling monster called a Kraken, and it has created an abode in an Ancient Cave. It is tunnelling under the city and is causing many buildings to collapse. Will you subdue it for me? I'll make it worth your while.<br /> You nod. </br>Excellent. I've arranged for you to get a sword. Go see the Blacksmith<br />";
 			mayor = true
 		} else if (msgArray[0] == "/mayor" && msgArray[2] == eval("fairfalcontownhall") && mayor) {
-			gamelog.innerHTML = gamelog.innerHTML + "What are you waiting for? Go see the blacksmith. He'll tell you the rest.<br />".	
+			gamelog.innerHTML = gamelog.innerHTML + "What are you waiting for? Go see the blacksmith. He'll tell you the rest.<br />".
 		} else if (msgArray[0] == "/mayor" && msgArray[2] !== eval("fairfalcontownhall")) {
 			gamelog.innerHTML = gamelog.innerHTML + "The mayor is not here..."
 		}
-		
+
 		if (msgArray[0] == "/kraken" && msgArray[2] == eval("cave")) {
 			console.log("RELEASE THE KRAKEN!")
-			//Fighting logic
+				//Fighting logic
 		} else {
 			gamelog.innerHTML = gamelog.innerHTML + "You can't do that."
 		}
