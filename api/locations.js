b6ffbb3f0fbdacdb00140cf2f8604465569e0615
Deep Sohelia <soheliadeep@gmail.com>
2014-07-09 14:00:56 +0100

    Create locations.js

---
+++ api/locations.js
@@ -0,0 +1,9 @@
+var fairfalcongate = {
+    name:  "Fairfalcon Gate",
+    North: "False",
+    South: "False",
+    East: "pathtofairfalcon",
+    West: "fairfalconmainstreet",
+    Description: "The gate leads out to a path which leads to a forest. Many merchants are by the gate, selling their wares. A path is to the East, The main street is to the West.",
+    Buildings: []
+};
